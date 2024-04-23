e7a1b89b085705fe495bab10660d1e744583849f
manithlakvidu2002 <lakvidu00@gmail.com>
2024-04-23 13:22:17 +0530

    Loging Page Created

---
+++ HelloShoes/src/main/java/lk/ijse/gdse66/HelloShoes/HelloShoesApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.HelloShoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShoesApplication.class, args);
+	}
+
+}
---
+++ HelloShoes/src/test/java/lk/ijse/gdse66/HelloShoes/HelloShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.HelloShoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
