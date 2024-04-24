6090e277d026fddec3763d7cb21b6bf98f322f0f
manithlakvidu2002 <lakvidu00@gmail.com>
2024-04-24 12:31:50 +0530

    New Update in Front-End

--- Front-End/controller/DashboardController.js
+++ Front-End/controller/DashboardController.js
@@ -1,33 +1,86 @@
 let login = document.getElementById("login")
 let signup = document.getElementById("signup")
 let forgetPassword = document.getElementById("forgetPassword")
+let navigate = document.getElementById("navigation-bar")
+let home = document.getElementById("home")
+/*let mngCustomer= document.getElementById("customer")
+let mngEmployee=document.getElementById("employee")
+let mngInventory =document.getElementById("inventory")
+let mngSale = document.getElementById("sale")
+let mngSupplier=document.getElementById("supplier")*/
 
 
 {
     login.style.display="block";
     signup.style.display="none";
     forgetPassword.style.display="none";
+    navigate.style.display="none";
+    home.style.display="none";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
+
 }
 function log(){
     login.style.display="block";
     signup.style.display="none";
     forgetPassword.style.display="none";
+    navigate.style.display="none";
+    home.style.display="none";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
 }
 function sign(){
     login.style.display="none";
     signup.style.display="block";
     forgetPassword.style.display="none";
-
-
+    navigate.style.display="none";
+    home.style.display="none";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
 }
 function signsave(){
     login.style.display="block";
     signup.style.display="none";
     forgetPassword.style.display="none";
-
+    navigate.style.display="none";
+    home.style.display="none";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
 }
 function newPassword(){
     login.style.display="none";
     signup.style.display="none";
     forgetPassword.style.display="block";
+    navigate.style.display="none";
+    home.style.display="none";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
+}
+
+function goHome(){
+    login.style.display="none";
+    signup.style.display="none";
+    forgetPassword.style.display="none";
+    navigate.style.display="block";
+    home.style.display="block";
+    /*mngCustomer.style.display="none";
+    mngEmployee.style.display="none";
+    mngSupplier.style.display="none";
+    mngSale.style.display="none";
+    mngInventory.style.display="none";*/
 }
