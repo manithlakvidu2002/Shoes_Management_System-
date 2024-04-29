f16105649409f3cb43d6eb9f368b30bfa3b5fff0
manithlakvidu2002 <lakvidu00@gmail.com>
2024-04-29 11:51:02 +0530

    Basic BackEnd Architecture Created

---
+++ HelloShoes/src/main/java/lk/ijse/gdse66/HelloShoes/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse66.HelloShoes.dto;
+
+/**
+ * @author: Manith Lakvidu,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+public class CustomerDTO {
+}
---
+++ HelloShoes/src/main/java/lk/ijse/gdse66/HelloShoes/service/CustomerService.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse66.HelloShoes.service;
+
+/**
+ * @author: Manith Lakvidu,
+ * @Runtime version: 11.0.11+9-b1341.60 amd64
+ **/
+
+public class CustomerService {
+}
